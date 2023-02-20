deddf0d343f67d236a0f2179e1525444442a653b
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2023-02-20 22:26:46 +0000

    Updated tabs and page content

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -5,12 +5,8 @@ export default function AboutMe() {
     <div>
       <h1>About Page</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+       I'm Cherry, a web developer with 3 years+ experience, serving UK-based clients in web design,
+       web maintanence and graphic design. Additonal skills include copywriting, video editng and digital marketing.
       </p>
     </div>
   );
--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -5,17 +5,11 @@ export default function ContactMe() {
     <div>
       <h1>Contact Me</h1>
       <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+        <ul>
+        <li>Email Me - cherrydakin@gmail.com</li>
+        <li>Visit my Github repo - <a href="https://github.com/cherry-aisha"> My Github Repo </a></li>
+        <li>Message Me on Instagram<a href="https://instagram.com/cherry.aisha?igshid=YmMyMTA2M2Y="> My Instagram </a></li>
+        </ul>
       </p>
     </div>
   );
