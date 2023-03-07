5d605c1c4b6dbc71928c5f5c8e31247c1ba65b11
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2023-03-07 00:52:56 +0000

    added avatar uodated color

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,14 +4,14 @@ import '../styles/Home.css';
 export default function Home() {
   return (
     <div className="hero-background">
-      <h1 className="home-header">Cherry Aisha's Web Developer's Portfolio</h1>
+      <h1 className="home-header">Cherry Code's</h1>
       <p>
       </p>
       <h2 className="home-footer"></h2>
       <div>
       <h1>About Me</h1>
       <span>
-        
+        <img src="../src/Assets/avatar.png" alt="Profile Avatar"></img>
       </span>
       <p>
        I'm Cherry, a web developer with 3 years+ experience, serving UK-based clients in web design,
