3ae020cfd70484f2da760b0d9262bcbefd5d4f3c
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2023-03-07 01:48:58 +0000

    Aligned avatar

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,15 +4,18 @@ import '../styles/Home.css';
 export default function Home() {
   return (
     <div className="hero-background">
-      <h1 className="home-header">Cherry Code's</h1>
-      <p>
-      </p>
-      <img src="../../../public/Assets/avatar.png" alt="Profile Avatar"></img>
+      <div>
+      <h1 className="home-header">Cherry Codes Portfolio</h1>
+      </div>
+      <div className="avatar-container">
+      <img className="avatar" src="/Assets/avatar.png" alt="Profile Avatar"></img>
+      </div>
       <div>
       <h1>About Me</h1>
       <p>
        I'm Cherry, a web developer with 3 years+ experience, serving UK-based clients in web design,
        web maintanence and graphic design. Additonal skills include copywriting, video editng and digital marketing.
+       I have completed the coding bootcamp with University of Birmingham!
       </p>
     </div>
     </div>
