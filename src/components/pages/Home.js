a0c56ce21c0d239a4079c6a9b5a7be8ca140e08a
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2023-03-09 23:51:08 +0000

    updated avatar and positioning

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,20 +4,19 @@ import '../styles/Home.css';
 export default function Home() {
   return (
     <div className="background">
-      <div>
-      
-      </div>
-      <div className="avatar-container">
-      <img className="avatar" src="../Assets/avatar.png" alt="Profile Avatar"></img>
-      </div>
-      <div className="about-me-container">
-      <h1 className="about-me home-header">About Me</h1>
+      <div className="">
+      <h1 className="">About Me</h1>
       <p>
        I'm Cherry, a web developer with 3 years+ experience, serving UK-based clients in web design,
        web maintanence and graphic design. Additonal skills include copywriting, video editng and digital marketing.
        I have completed the coding bootcamp with University of Birmingham!
       </p>
     </div>
+      <aside className="avatarContainer">
+      <a href="#home" target="_blank">
+            <img className="avatar" src="https://raw.githubusercontent.com/cherry-aisha/week-20-react-portfolio/89159933559023e2fddf1ccce486055cd18fe7c3/public/Assets/avatar.png" alt="GitHub"/>
+            </a>
+      </aside>
     </div>
   );
 }
